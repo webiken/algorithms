3af61fcb2b47ec7b7f27e09158e4ba15e5003e6d
Mohamed <sam@webiken.net>
2014-12-06 18:26:13 -0500

    added the solution I found in Java

---
+++ Solution1.java
@@ -0,0 +1,26 @@
+public class Solution1 {
+    public static void main(String[] args) {
+    int A[]= {0, 1, 3, -2, 0, 1, 0, -3, 2, 3};
+    int depth = 0;
+
+    int P = -1, Q = -1, R = -1;
+    P = 0;
+
+    for (int i = 1; i < A.length; i++)
+    {
+        if (Q < 0 && A[i] >= A[i-1]) Q = i-1;
+
+        if ((Q >= 0 && R < 0) && 
+            (A[i] <= A[i-1] || i + 1 == A.length))
+        {
+            R = i-1;
+            System.out.println(P+"  "+Q+"  "+R);
+            depth = Math.max(depth, Math.min(A[P]-A[Q], A[R]-A[Q]));
+            P = i - 1; 
+            Q = R = -1;
+        }
+    }
+    if (depth == 0) depth = -1;
+    System.out.println("Depth: "+depth);    
+    }    
+}
