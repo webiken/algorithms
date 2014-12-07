834ce376324090c62ad3300a97f876336a845c7e
Mohamed <sam@webiken.net>
2014-12-07 12:39:41 -0500

    added debug statements to calculate O notation

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
-A = [0, 1, 3, -2, 0, 1, 0, -3, 2, 3]
 
 def insertion_sort(_array):
+	global count
 	for i in range(len(_array)):
+		count = count + 1
 		j = i
+		print 'j={} j-1={}..{} {}'.format(j, j-1, _array[j], _array[j-1])
 		while ((j > 0) and (_array[j] < _array[j-1])):
+			count = count + 1
+			print 'j={} j-1={}>>{} {}'.format(j, j-1, _array[j], _array[j-1])
 			_array[j], _array[j-1] = _array[j-1], _array[j]
 			j = j - 1
 	return _array
 
+
+A = [3, 2, 1]
+count = 0
 print insertion_sort(A)
+print '{}'.format(count)
+
+A = [4, 3, 2, 1]
+count = 0
+print insertion_sort(A)
+print '{}'.format(count)
+
+A = [5, 4, 3, 2, 1]
+count = 0
+print insertion_sort(A)
+print '{}'.format(count)
